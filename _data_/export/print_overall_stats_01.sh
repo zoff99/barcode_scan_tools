e242e34795aa28005c1f8d783cb429907877d80b
zoff99 <zoff@zoff.cc>
2019-12-05 19:41:37 +0100

    add new script

--- _data_/export/print_overall_stats_01.sh
+++ _data_/export/print_overall_stats_01.sh
@@ -14,7 +14,7 @@ printf 'select distinct(d.code), c.description as desc, sum(d.anzahl) as anzahl
 from drinks d, ean_codes c
 where d.code=c.code
 and c.valid=1
-and d.datum BETWEEN "2019-10-10" AND "2200-01-01"
+and d.datum BETWEEN "2019-10-05" AND "2200-01-01"
 group by d.code
 order by anzahl desc
 \n'| sqlite3 -header "$sqldb_file"|grep -v '^$'|cut -d'|' -f 2-|awk -F"|" '{ printf "%-40s %s\n",$1,$2 }'
