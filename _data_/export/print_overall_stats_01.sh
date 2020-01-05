58f3269687b3b8c60ee6bffa1a1ae04e89f11298
zoff99 <zoff@zoff.cc>
2020-01-05 20:05:48 +0100

    add new script

--- _data_/export/print_overall_stats_01.sh
+++ _data_/export/print_overall_stats_01.sh
@@ -9,14 +9,57 @@ export _HOME2_
 _HOME_=$(cd $_HOME2_;pwd)
 export _HOME_
 
+
+days_span=$(printf 'select julianday(max(d.datum)) - julianday(min(d.datum)) as _days_
+from drinks d, ean_codes c
+where d.code=c.code
+and c.valid=1
+and d.datum BETWEEN "2019-10-05" AND "2200-01-01"
+\n'| sqlite3 -header "$sqldb_file"|grep -v '^$'|tail -1|tr -dc '[0-9.,]' 2>/dev/null|tr ',' '.')
+
 echo ""
-printf 'select distinct(d.code), c.description as desc, sum(d.anzahl) as anzahl
+echo ""
+echo ""
+echo ""
+
+echo "====================================================================="
+echo "Verbrauch pro 30 Tage (mit Daten der letzten $days_span days Tage)"
+echo "====================================================================="
+
+echo ""
+printf 'select distinct(d.code), c.description as desc, c.per_crate as pc,
+CAST(
+(
+((
+(sum(d.anzahl) * 30.0 / '"$days_span"' / c.per_crate )
+) * 10
++ 5 )
+/ 10 )
+as INTEGER
+)
+as kisten
 from drinks d, ean_codes c
 where d.code=c.code
 and c.valid=1
 and d.datum BETWEEN "2019-10-05" AND "2200-01-01"
 group by d.code
-order by anzahl desc
-\n'| sqlite3 -header "$sqldb_file"|grep -v '^$'|cut -d'|' -f 2-|awk -F"|" '{ printf "%-40s %s\n",$1,$2 }'
+order by kisten desc
+\n'| sqlite3 -header "$sqldb_file"|grep -v '^$'|cut -d'|' -f 2-|awk -F"|" '{ printf "%-45s %s\n",$1,$3 }'
+echo ""
+
+echo "====================================================================="
+echo "Verbrauch der letzten $days_span days Tage"
+echo "====================================================================="
+
+
+echo ""
+printf 'select distinct(d.code), c.description as desc, sum(d.anzahl) as flaschen
+from drinks d, ean_codes c
+where d.code=c.code
+and c.valid=1
+and d.datum BETWEEN "2019-10-05" AND "2200-01-01"
+group by d.code
+order by flaschen desc
+\n'| sqlite3 -header "$sqldb_file"|grep -v '^$'|cut -d'|' -f 2-|awk -F"|" '{ printf "%-45s %s\n",$1,$2 }'
 echo ""
 
